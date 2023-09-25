6dd3b9f475de7c595c73e49c55b726eb1ea5c41b
alifeee <alifeeerenn@hotmail.com>
2023-09-25 14:53:31 +0100

    add local build script

---
+++ package_windows.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cp build/Release/plugin.dll plugin.dll
+cp mumble/manifest.xml manifest.xml
+# remove <plugin os="linux" arch="x64">libplugin.so</plugin> from manifest.xml
+sed -i '/<plugin os="linux" arch="x64">libplugin.so<\/plugin>/d' manifest.xml
+zip -MM -r factorio.mumble_plugin plugin.dll manifest.xml
+mv factorio.mumble_plugin build/factorio.mumble_plugin
+rm plugin.dll manifest.xml
