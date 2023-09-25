48694f7766b2927db57e0d4817598cfb6bee8df4
alifeee <alifeeerenn@hotmail.com>
2023-09-25 14:58:48 +0100

    add info to packager

--- package_windows.sh
+++ package_windows.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "Building Mumble plugin for Windows"
 cp build/Release/plugin.dll plugin.dll
 cp mumble/manifest.xml manifest.xml
 # remove <plugin os="linux" arch="x64">libplugin.so</plugin> from manifest.xml
@@ -6,3 +7,4 @@ sed -i '/<plugin os="linux" arch="x64">libplugin.so<\/plugin>/d' manifest.xml
 zip -MM -r factorio.mumble_plugin plugin.dll manifest.xml
 mv factorio.mumble_plugin build/factorio.mumble_plugin
 rm plugin.dll manifest.xml
+echo "Done -> build/factorio.mumble_plugin"
