0a22c11fbb3e5d25d2f4bb6251c3eda9064cd058
alifeee <alifeeerenn@hotmail.com>
2023-09-26 00:47:09 +0100

    remove intermediate assignment in Lua script

--- factorio/control.lua
+++ factorio/control.lua
@@ -1,12 +1,8 @@
 script.on_nth_tick(5, function()
     for index, player in pairs(game.connected_players) do
-        local info = "XYZ, Player, sUrface, Server\n"
-        info = info .. "x: " .. player.position.x .. "\n"
-        info = info .. "y: " .. player.position.y .. "\n"
-        info = info .. "z: " .. 0 .. "\n"
-        info = info .. "p: " .. index .. "\n"
-        info = info .. "u: " .. player.surface.index .. "\n"
-        info = info .. "s: " .. game.get_player(1).name .. "\n"
+        local info = "XYZ, Player, sUrface, Server\n" .. "x: " .. player.position.x .. "\n" .. "y: " ..
+                         player.position.y .. "\n" .. "z: " .. 0 .. "\n" .. "p: " .. index .. "\n" .. "u: " ..
+                         player.surface.index .. "\n" .. "s: " .. game.get_player(1).name .. "\n"
         game.write_file("mumble_positional-audio_information.txt", info, false, player.index)
     end
 end)
